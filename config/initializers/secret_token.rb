e99c19963e017708699a533d1bf1a452d551a351
Phillip Lackey <phillip.lackey@athlonsports.com>
2013-03-26 12:35:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeatTest::Application.config.secret_token = '5462e64462b28d57d1d01c0795725d9ffaba7cf54d3e9f8b229f27cb7afce8afd5eafab564f9f132a5c423aab44cdbe2d9f2912e360a4da4c83b10b6eb168464'
