38482e48ea8259cfaa4c82c11f20ef8f1fef0703
Phillip Lackey <phillip.lackey@athlonsports.com>
2013-03-26 23:07:20 -0500

    Added additional styles with neat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,22 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require selectivizr
 //= require_tree .
+
+
+$(document).ready(function() {
+	
+	$(function() {
+	  if ($.browser.msie && $.browser.version.substr(0,1)<7)
+	  {
+		$('li').has('ul').mouseover(function(){
+			$(this).children('ul').css('visibility','visible');
+			}).mouseout(function(){
+			$(this).children('ul').css('visibility','hidden');
+			})
+	  }
+	}); 
+	
+	
+});
